4f6fa2e92dd81c5ca6bd6205290c539fc3737a65
Andy Rodriguez <andyrodriguez0@gmail.com>
2023-08-30 16:30:33 -0400

    create result interface and refactor movies service

--- server/src/movies/movies.service.ts
+++ server/src/movies/movies.service.ts
@@ -1,22 +1,41 @@
-import { Injectable } from '@nestjs/common';
-
-const apiKey = '855a13a8bd82d244b0f38aad5fea55ca';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Result  } from './result.interface';
 
 @Injectable()
 export class MoviesService {
   async getPopular() {
+    const apiKey = process.env.API_KEY;
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
     const response = await fetch(url);
+    if (!response.ok) {throw new InternalServerErrorException('There was an error retrieving the popular movies. Please try again later.')}
     const data = await response.json();
-    const popular = data['results'].map(result => {return {movieId: result.id, title: result.title, releaseDate: result.release_date, posterImage: result.poster_path, description: result.overview, averageRating: result.vote_average}});
-    return popular;
+    const results = data['results'];
+    const processed = this.processResults(results);
+    return processed;
   }
 
   async getSearch(query: string) {
+    const apiKey = process.env.API_KEY;
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&language=en-US&page=1`;
     const response = await fetch(url);
+    if (!response.ok) {throw new InternalServerErrorException('There was an error searching for the movies. Please try again later.')}
     const data = await response.json();
-    const search = data['results'].map(result => {return {movieId: result.id, title: result.title, releaseDate: result.release_date, posterImage: result.poster_path, description: result.overview, averageRating: result.vote_average}});
-    return search;
+    const results = data['results'];
+    const processed = this.processResults(results);
+    return processed;
+  }
+
+  processResults(results: Array<Result>) {
+    const processed = results.map((result: Result) => {
+      return {
+        movieId: result.id,
+        title: result.title,
+        releaseDate: result.release_date,
+        posterImage: result.poster_path,
+        description: result.overview,
+        averageRating: result.vote_average
+      }
+    });
+    return processed;
   }
 }
